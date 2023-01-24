a77bab7092c9f5b93d59bca15b4e135b7dc2ebd4
Phắc Nguyễn <phapdeptrai2k2@gmail.com>
2023-01-24 19:47:11 +0700

    initialize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from model.model import predict
 
 app = FastAPI()
 
@@ -7,21 +8,28 @@ class Msg(BaseModel):
     msg: str
 
 
+
+
+
+class Input(BaseModel):
+    adults: int
+    children: int
+    parking: int
+    repeated_guest: int
+    lead_time: int
+    weekend: int
+    weekday: int
+    prev_cancel: int
+
+
+class PredictionOut(BaseModel):
+    is_canceled: int
+
 @app.get("/")
 async def root():
     return {"message": "Hello World. Welcome to FastAPI!"}
 
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.post("/predict")
+def WeatherResult(payload: Input):
+    is_canceled = predict(payload.adults, payload.children, payload.parking, payload.repeated_guest, payload.lead_time, payload.weekend, payload.weekday, payload.prev_cancel)
+    return {"Result": is_canceled}
