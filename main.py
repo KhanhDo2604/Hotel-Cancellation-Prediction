a107a7d28b4d29c9a4d571718ea7efc2639fb5c8
Phắc Nguyễn <phapdeptrai2k2@gmail.com>
2023-01-24 20:48:36 +0700

    fix bug

--- main.py
+++ main.py
@@ -2,34 +2,43 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from model.model import predict
 
+
 app = FastAPI()
-
-class Msg(BaseModel):
-    msg: str
-
-
-
 
 
 class Input(BaseModel):
-    adults: int
-    children: int
-    parking: int
-    repeated_guest: int
+    no_of_adults: int
+    no_of_children: int
+    no_of_weekend_nights: int
+    no_of_week_nights: int
+    required_car_parking_space: int
     lead_time: int
-    weekend: int
-    weekday: int
-    prev_cancel: int
+    repeated_guest: int
+    no_of_previous_cancellations: int
+    no_of_previous_bookings_not_canceled: int
 
 
 class PredictionOut(BaseModel):
     is_canceled: int
 
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+def home():
+    return {"health_check": "OK"}
+
 
 @app.post("/predict")
 def WeatherResult(payload: Input):
-    is_canceled = predict(payload.adults, payload.children, payload.parking, payload.repeated_guest, payload.lead_time, payload.weekend, payload.weekday, payload.prev_cancel)
-    return {"Result": is_canceled}
+    is_canceled = predict(
+        payload.no_of_adults, 
+        payload.no_of_children, 
+        payload.no_of_weekend_nights, 
+        payload.no_of_week_nights, 
+        payload.required_car_parking_space, 
+        payload.lead_time, 
+        payload.repeated_guest, 
+        payload.no_of_previous_cancellations, 
+        payload.no_of_previous_bookings_not_canceled
+    )
+
+    return {"Result": int(str(is_canceled))}
