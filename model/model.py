1aa7324a740119b8d5f4cf649be2c0734fe2f340
Đỗ Phạm Huy Khánh <20521442@gm.uit.edu.vn>
2023-01-24 20:54:00 +0700

    fix bug deploy server

--- model/model.py
+++ model/model.py
@@ -5,7 +5,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent
 with open(f"{BASE_DIR}/model.pkl", "rb") as f:
     clf = pickle.load(f)
 
-def predict(adults, children, parking, repeated_guest, lead_time, weekend, weekday, prev_cancel):
-    input = [adults, children, parking, repeated_guest, lead_time, weekend, weekday, prev_cancel]
+def predict(no_of_adults, no_of_children, no_of_weekend_nights, no_of_week_nights, required_car_parking_space, lead_time, repeated_guest, no_of_previous_cancellations, no_of_previous_bookings_not_canceled):
+    input = [no_of_adults, no_of_children, no_of_weekend_nights, no_of_week_nights, required_car_parking_space, lead_time, repeated_guest, no_of_previous_cancellations, no_of_previous_bookings_not_canceled]
     return clf.predict([input])[0]
+
+
 
